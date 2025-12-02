8a91fc9c07a8cf1aeadc778c79229cb9fb759e75
Elias Josué HAJJAR LLAUQUEN <elias-josue.hajjar-llauquen@epitech.eu>
2025-12-02 19:45:20 +0100

    [ADD] Base client structure added

---
+++ client/include/Systems/RenderSystem.hpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2025
+** Air-Trap
+** File description:
+** RenderSystem
+*/
+
+#pragma once
+// #include <SFML/Graphics.hpp>
+// #include "ecs/System.hpp"
+// #include "ecs/components/PositionComponent.hpp"
+// #include "ecs/components/SpriteComponent.hpp"
