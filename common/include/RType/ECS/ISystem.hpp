d7ec1f3676a51f0f96c2b397b91eac46d7e91552
Robin Toillon <robin.toillon@epitech.eu>
2025-12-08 21:21:08 +0100

    [ADD] ISystem interface declaration

--- common/include/RType/ECS/ISystem.hpp
+++ common/include/RType/ECS/ISystem.hpp
@@ -8,11 +8,15 @@
 #ifndef RTYPE_ISYSTEM_HPP_
     #define RTYPE_ISYSTEM_HPP_
 
+    #include "RType/ECS/Registry.hpp"
+
 namespace rtp::ecs
 {
     class ISystem {
         public:
-            
+            virtual ~ISystem() noexcept = default;
+
+            virtual void update(Registry &registry, float deltaTime) = 0;
     };
 }
 
