8206138387bb2a476743793ed34f3b984454e6f6
Elias Josué HAJJAR LLAUQUEN <elias-josue.hajjar-llauquen@epitech.eu>
2025-12-14 16:22:23 +0100

    [ADD] add multiple features
    • InputSystem.hpp: Add control flow
    • Transform.hpp: Modify code
    
    Scope: client
    Files modified: 2

--- client/include/Systems/InputSystem.hpp
+++ client/include/Systems/InputSystem.hpp
@@ -22,21 +22,19 @@ namespace rtp::client {
         explicit InputSystem(rtp::ecs::Registry& r) : _r(r) {}
 
         void update(float dt) override {
-            auto view = _r.zipView<rtp::ecs::components::Velocity, rtp::ecs::components::Controllable>();
+            auto view = _r.zipView<rtp::ecs::components::Transform, rtp::ecs::components::Controllable>();
 
-            for (auto&& [vel, _] : view) {
-                vel.direction = {0.0f, 0.0f};
-
+            for (auto&& [transform, _] : view) {
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) {
-                    vel.direction.y -= 1.0f;
+                    transform.position.y -= 1.0f;
                     rtp::log::info("Up key pressed");
                 }
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
-                    vel.direction.y += 1.0f;
+                    transform.position.y += 1.0f;
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
-                    vel.direction.x -= 1.0f;
+                    transform.position.x -= 1.0f;
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
-                    vel.direction.x += 1.0f;
+                    transform.position.x += 1.0f;
             }
         }
     private:
