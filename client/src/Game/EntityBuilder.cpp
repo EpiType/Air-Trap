8f735e961f1ee33265d6764bcf53885bfdb0e116
Enzo <enzo.lobato-coutinho@epitech.eu>
2025-12-16 02:28:25 +0100

    [ADD] modifiying EnnemyBuilder to EntityBuilder

--- client/src/Game/EntityBuilder.cpp
+++ client/src/Game/EntityBuilder.cpp
@@ -5,14 +5,14 @@
 ** EnemyBuilder implementation
 */
 
-#include "Game/EnemyBuilder.hpp"
+#include "Game/EntityBuilder.hpp"
 
 namespace Client::Game {
 
-EnemyBuilder::EnemyBuilder(rtp::ecs::Registry &registry)
+EntityBuilder::EntityBuilder(rtp::ecs::Registry &registry)
     : _registry(registry) {}
 
-auto EnemyBuilder::spawn(const EnemyTemplate &t)
+auto EntityBuilder::spawn(const EntityTemplate &t)
     -> std::expected<rtp::ecs::Entity, rtp::Error>
 {
     auto e = _registry.spawnEntity();
@@ -36,12 +36,12 @@ auto EnemyBuilder::spawn(const EnemyTemplate &t)
     return entity;
 }
 
-void EnemyBuilder::kill(rtp::ecs::Entity entity)
+void EntityBuilder::kill(rtp::ecs::Entity entity)
 {
     _registry.killEntity(entity);
 }
 
-void EnemyBuilder::update(rtp::ecs::Entity entity, const EnemyTemplate &t)
+void EntityBuilder::update(rtp::ecs::Entity entity, const EntityTemplate &t)
 {
     if (auto arr = _registry.getComponents<rtp::ecs::components::Transform>(); arr) {
         auto &sa = arr->get();
