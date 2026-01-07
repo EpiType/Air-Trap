2b7b0ec019da1a404d097d3fa01c0f2ecb08b1a2
Elias Josué HAJJAR LLAUQUEN <elias-josue.hajjar-llauquen@epitech.eu>
2026-01-07 06:12:18 +0100

    [ADD] add multiple features
    • Modified ClientNetworkSystem.hpp
    • Modified Application.cpp
    • Modified ClientNetworkSystem.cpp
    • Modified RoomId.hpp
    • Modified Packet.hpp
    • Modified Packet.tpp
    • Modified Room.hpp
    • Modified EnemyAISystem.hpp
    • Modified EntitySystem.hpp
    • Modified MovementSystem.hpp
    • Modified PlayerMouvementSystem.hpp
    • Modified RoomSystem.hpp
    • Modified GameManager.cpp
    • Modified Room.cpp
    • Modified EnemyAISystem.cpp
    • Modified EntitySystem.cpp
    • Modified MovementSystem.cpp
    • PlayerMouvementSystem.cpp: Add control flow + remove functions
    
    Scope: client
    Files modified: 18

--- server/src/Systems/PlayerMouvementSystem.cpp
+++ server/src/Systems/PlayerMouvementSystem.cpp
@@ -1,8 +1,10 @@
 /**
  * File   : PlayerMouvementSystem.cpp
  * License: MIT
- * Author : Elias Josué HAJJAR LLAUQUEN <elias-josue.hajjar-llauquen@epitech.eu>
- * Date   : 11/12/2025
+ * Author : Elias
+ * Josué HAJJAR LLAUQUEN <elias-josue.hajjar-llauquen@epitech.eu>
+ * Date   :
+ * 11/12/2025
  */
 
 #include "Systems/PlayerMouvementSystem.hpp"
@@ -13,35 +15,44 @@ namespace rtp::server
     // Public API
     //////////////////////////////////////////////////////////////////////////
 
-    PlayerMouvementSystem::PlayerMouvementSystem(rtp::ecs::Registry& registry)
-        : _registry(registry) {}
+    PlayerMouvementSystem::PlayerMouvementSystem(rtp::ecs::Registry &registry)
+        : _registry(registry)
+    {
+    }
 
     void PlayerMouvementSystem::update(float dt)
     {
-        (void) dt;
-        auto view = _registry.zipView<
-            rtp::ecs::components::Transform,
-            rtp::ecs::components::Velocity,
-            rtp::ecs::components::server::InputComponent,
-            rtp::ecs::components::EntityType
-        >();
+        (void)dt;
+        auto view =
+            _registry.zipView<rtp::ecs::components::Transform,
+                              rtp::ecs::components::Velocity,
+                              rtp::ecs::components::server::InputComponent,
+                              rtp::ecs::components::EntityType>();
 
         constexpr float speed = 200.f;
 
-        for (auto&& [tf, vel, input, type] : view) {
+        for (auto &&[tf, vel, input, type] : view) {
             if (type.type != rtp::net::EntityType::Player)
                 continue;
 
             float dx = 0.f, dy = 0.f;
-            using Bits = rtp::ecs::components::server::InputComponent::InputBits;
+            using Bits =
+                rtp::ecs::components::server::InputComponent::InputBits;
 
-            if (input.mask & Bits::MoveUp)    dy -= 1.f;
-            if (input.mask & Bits::MoveDown)  dy += 1.f;
-            if (input.mask & Bits::MoveLeft)  dx -= 1.f;
-            if (input.mask & Bits::MoveRight) dx += 1.f;
+            if (input.mask & Bits::MoveUp)
+                dy -= 1.f;
+            if (input.mask & Bits::MoveDown)
+                dy += 1.f;
+            if (input.mask & Bits::MoveLeft)
+                dx -= 1.f;
+            if (input.mask & Bits::MoveRight)
+                dx += 1.f;
 
-            const float len = std::sqrt(dx*dx + dy*dy);
-            if (len > 0.f) { dx /= len; dy /= len; }
+            const float len = std::sqrt(dx * dx + dy * dy);
+            if (len > 0.f) {
+                dx /= len;
+                dy /= len;
+            }
 
             vel.velocity.x = dx * speed;
             vel.velocity.y = dy * speed;
